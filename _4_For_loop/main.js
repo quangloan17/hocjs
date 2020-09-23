3629601791a037eb49389231c072eba25e0d5939
Quang Nguyen <quang.cvtot@gmail.com>
2020-09-23 15:45:21 +0700

    some message

---
+++ _4_For_loop/main.js
@@ -0,0 +1,8 @@
+//For loop
+
+// B1: Tạo biến var = 1, Chỉ chạy 1 lần và sau ko quay lại
+// B2: i < 1000, hơn 1 đến 1000 break
+// B3: Toán tử i++ sẽ tăng 1 đơn vị i
+for (var i = 1; i <= 1000; i++) {
+    console.log(i);
+}
